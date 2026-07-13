d39ad0d4aed5af6153d4a535953cb5ea16562e0c
TARUN-2305 <tarunr.cs23@rvce.edu.in>
2026-07-13 20:34:14 +0530

    Clean up test suite by removing synthetic/redundant padding from pairs/ directory

--- fcv/tests/pairs/023_generic_test_23/interface.f90
+++
@@ -1,6 +0,0 @@
-interface
-          subroutine test(x) bind(c, name="test")
-            import :: c_int
-            integer(c_int), value :: x
-          end subroutine test
-        end interface
